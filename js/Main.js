f72636e745c1c6a7ed89d151edca3460040eaeab
issei-toyomoto <120348418+issei-toyomoto@users.noreply.github.com>
2025-02-12 11:46:44 +0900

    Update Main.js

--- js/Main.js
+++ js/Main.js
@@ -14,16 +14,21 @@ var render = Render.create({
     engine: engine
 });
 
+//柱の長さをランダムに変更するための変数に値を代入
+let h_pool = Math.floor(Math.random() * (450 - 100)) + 100;
+
 // 静止オブジェクト(地面)
 const ground = Bodies.rectangle(400, 585, 800, 30, { isStatic: true });
 
 // 可動オブジェクト（円）
 const circle = Bodies.circle(180, 10 - 70, 25, { friction: 0.01 });
+
+
 // 可動オブジェクト（柱）
-//const pool = Bodies.rectangle();
+const pool = Bodies.rectangle(400,30,60,h_pool);
 
 // オブジェクトの追加
-Composite.add(engine.world, [ground, circle]);
+Composite.add(engine.world, [ground, circle,pool]);
 
 // run the renderer
 Render.run(render);
@@ -34,34 +39,13 @@ var runner = Runner.create();
 // run the engine
 Runner.run(runner, engine);
 
-document.body.addEventListener('keydown',
+//キーが離された時中の処理をする
+document.body.addEventListener('keyup',
     event => {
+        //Wが離された時
         if (event.key === 'w') {
             circle.force.y = -0.04;
         }
 });
 
-document.body.addEventListener('keydown',
-    event => {
-        if (event.key === 'a') {
-            circle.force.x = -0.05;
-        }
-});
-
-document.body.addEventListener('keydown',
-    event => {
-        if (event.key === 's') {
-            circle.force.y = 0.05;
-        }
-});
-
-document.body.addEventListener('keydown',
-    event => {
-        if (event.key === 'd') {
-            circle.force.x = 0.1;
-        }
-});
-
-/*setInterval(()=>{
-    document.querySelector('#debugkanato').innerHTML = square.force.y;
-},10)*/
+Matter
